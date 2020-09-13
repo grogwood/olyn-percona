e9e187fca8ba97c754f71257827446ed0c0adfef
Scott Richardson <dev@grogwood.com>
2020-09-12 22:58:26 -0700

    "Bootstrapper" server config moved to top level

--- recipes/security.rb
+++ recipes/security.rb
@@ -30,6 +30,6 @@ execute 'mysql_secure_script' do
           "touch #{Chef::Config[:file_cache_path]}/percona.sql.hardening.lock"
   creates "#{Chef::Config[:file_cache_path]}/percona.sql.hardening.lock"
   action :run
-  only_if { local_server[:options][:percona][:bootstrapper] }
+  only_if { local_server[:options][:bootstrapper] }
   sensitive true
 end
